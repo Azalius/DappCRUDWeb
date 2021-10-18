0e4d52c5048ffb2c730135f5a10ad64b3e9faa9a
Azalius <mateo1998@live.fr>
2021-10-18 22:12:53 +0200

    Connection BC ok, la page login affiche le MOTD de la BC

--- web/js/index.js
+++ web/js/index.js
@@ -1,2 +1,46 @@
+let web3;
+let contractAdress = '0x9Bc2dF8dc5Eb26D2cAd08766EC0c60674c0c7ABD';
+
+
+
+
+try {            
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+} catch (error) {
+  alert(error)
+}
+
+
+
+async function setupMotd(){
+  await $.getJSON("contracts/MOTD.json", function(result) {            
+    abi = result.abi                
+  });
+  contractMotd = new web3.eth.Contract(
+    abi,
+     contractAdress
+  );
+  motd = contractMotd.methods.getMotd().call().then(function (result) {                
+    $('#motdField').html(result);
+  })
+}
+
+setupMotd()
+
+async function requestWalletConnect(){
+  if (window.ethereum != null) {
+    state.web3 = new Web3(window.ethereum)
+    try {
+      // Request account access if needed
+      await window.ethereum.enable()
+      // Acccounts now exposed
+    } catch (error) {
+      // User denied account access...
+    }
+  }  
+}
+
+
+
 
 
